f83aee3895c2fdcbd69fdcc5f2d8cc3f5160f324
1000007201 <nishant.sharma5504@gmail.com>
2022-01-31 19:35:27 +0530

    [Nishant] Add:Day of Week Program Done

---
+++ Calender.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgram
+{
+    public class Calender
+    {
+        public static int DayOfWeek(int month ,int day,int year)
+        {
+            int year_not = year - (14 - month) / 12;
+            int x = year_not + year_not / 4 - year_not / 100 + year_not / 400;
+            int month_not = month + 12 * ((14 - month) / 12) - 2;
+            int day_not = (day + x + 31 * month_not / 12) % 7;
+            return (int)day_not;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,7 +9,8 @@ while (flag)
         "3.Prime Number\n"+
         "4.Reverse Number\n"+
         "5.Vending Machine\n"+
-        "6.Exit");
+        "6.Day Of Week\n"+
+        "7.Exit");
     int option = Convert.ToInt32(Console.ReadLine());
     switch (option)
     {
@@ -34,6 +35,35 @@ while (flag)
             note.NoteCounter();
             break;
         case 6:
+            int week = Calender.DayOfWeek(31, 1, 2022);
+            switch (week)
+            {
+                case 0:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+            }
+
+
+            break;
+        case 7:
             flag = false;
             break;
     }
