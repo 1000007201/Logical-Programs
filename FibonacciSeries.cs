08d2718603b8b7b9d70b425633309c8ad50f1715
1000007201 <nishant.sharma5504@gmail.com>
2022-01-31 13:45:17 +0530

    [Nishant] Add:Fibonacci Series done

---
+++ FibonacciSeries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgram
+{
+    public class FibonacciSeries
+    {
+        public void Fibonacci()
+        {
+            Console.WriteLine("Enter number of terms you need:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int first_num = 1, second_num = 1,third_num;
+            Console.Write(first_num+"\t"+second_num);
+            for (int i = 0; i < number; i++)
+            {
+                third_num = first_num + second_num;
+                first_num = second_num;
+                second_num = third_num;
+                Console.Write("\t"+third_num);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FunctionalProgram;
+
+bool flag = true;
+while(flag)
+{
+    Console.WriteLine("Enter Value:\n1.Fibonacci Series\n" +
+        "2.Exit");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            FibonacciSeries fibonacci = new FibonacciSeries();
+            fibonacci.Fibonacci();
+            break;
+        case 2:
+            flag = false;
+            break;
+    }
+
+}
+
